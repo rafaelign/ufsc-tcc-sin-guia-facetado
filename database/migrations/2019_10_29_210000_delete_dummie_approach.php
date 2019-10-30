ddcb059343f01deb5f43071286f4d6e6cf9c68a8
Nathália <natilizbrito@gmail.com>
2019-10-30 05:29:06 -0200

    Migraçao para excluir abordagem exemplo

---
+++ database/migrations/2019_10_29_210000_delete_dummie_approach.php
@@ -0,0 +1,24 @@
+<?php
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Carbon\Carbon;
+class DeleteDummieApproach extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+       $dummieapproach = DB::table('approaches')
+            ->select(['id'])
+            ->where('slug', 'abordagem-exemplo')
+            ->first();
+
+        DB::table('approaches')
+            ->delete($dummieapproach->id);
+    }
+
+}
