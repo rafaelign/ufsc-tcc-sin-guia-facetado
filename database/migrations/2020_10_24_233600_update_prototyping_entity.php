7cb32e96bc85c9b3147a508d1e35b8858959cca0
Rafael Ignacio <rafael.nacio@gmail.com>
2020-11-08 19:44:19 -0300

    Fix migration name

--- database/migrations/2020_10_24_233600_update_prototyping_entity.php
+++ database/migrations/2020_10_24_233600_update_prototyping_entity.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 use Carbon\Carbon;
 
-class SeedEntitiesTableWithPrototyping extends Migration
+class UpdatePrototypingEntity extends Migration
 {
     /**
      * Run the migrations.
